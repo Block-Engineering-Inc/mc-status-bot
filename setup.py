84a7e098db7a20fde095e40ea10682874a03bc38
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2022-05-15 11:58:18 -0300

    Remove updater scripts

--- setup.py
+++ setup.py
@@ -40,25 +40,6 @@ def y_n(q):
         elif ri.lower() in ["no", "n"]:
             return False
 
-
-def update_deps():
-    print("Attempting to update dependencies...")
-
-    try:
-        subprocess.check_call(
-            '"{}" -m pip install --no-warn-script-location --user -U -r requirements.txt'.format(
-                sys.executable
-            ),
-            shell=True,
-        )
-    except subprocess.CalledProcessError:
-        raise OSError(
-            "Could not update dependencies. You will need to run '\"{0}\" -m pip install -U -r requirements.txt' yourself.".format(
-                sys.executable
-            )
-        )
-
-
 def get_info(question, *, default=None, optional=True):
     default_text = f" [{default}]" if default else " []"
     default_text = default_text if optional else ""
@@ -317,56 +298,6 @@ def run_setup():
 def main():
     print("Starting...")
 
-    # Make sure that we're in a Git repository
-    if not os.path.isdir(".git"):
-        raise EnvironmentError("This isn't a Git repository.")
-
-    # Make sure that we can actually use Git on the command line
-    # because some people install Git Bash without allowing access to Windows CMD
-    try:
-        subprocess.check_call("git --version", shell=True, stdout=subprocess.DEVNULL)
-    except subprocess.CalledProcessError:
-        raise EnvironmentError(
-            "Couldn't use Git on the CLI. You will need to run 'git pull' yourself."
-        )
-
-    print("Passed Git checks...")
-
-    # Check that the current working directory is clean
-    sp = subprocess.check_output(
-        "git status --porcelain", shell=True, universal_newlines=True
-    )
-    if sp:
-        ohno = y_n(
-            "You have modified files that are tracked by Git (e.g the bot's source files).\n"
-            "Should we try resetting the repo? You will lose local modifications."
-        )
-        if ohno:
-            try:
-                subprocess.check_call("git reset --hard", shell=True)
-            except subprocess.CalledProcessError:
-                raise OSError("Could not reset the directory to a clean state.")
-        else:
-            wowee = y_n(
-                "OK, skipping bot update. Do you still want to update dependencies?"
-            )
-            if wowee:
-                update_deps()
-
-            run_setup()
-            print("Done. You may now run the bot.")
-            return
-
-    print("Checking if we need to update the bot...")
-
-    try:
-        subprocess.check_call("git pull", shell=True)
-    except subprocess.CalledProcessError:
-        raise OSError(
-            "Could not update the bot. You will need to run 'git pull' yourself."
-        )
-
-    update_deps()
     run_setup()
 
     print("Done. You may now run the bot.")
